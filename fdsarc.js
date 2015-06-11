5bd0c2be3c0079dba9f835d54813f5b2250bfe5b
xamprank <xamprank@gmail.com>
2015-06-11 12:08:43 +0000

    Edit

--- fdsarc.js
+++ fdsarc.js
@@ -37,8 +37,6 @@ document.write('</div>');
 document.write('<table width="100%" border="0" cellpadding="0" cellspacing="0" style="margin:0; padding:0; color:#666; font-family:Tahoma,Arial,sans-serif; font-size:11px; line-height:13px;"><tbody><tr>');
 document.write('<td align="right" width="100%"><div id="fdsarc_optionDiv" style="vertical-align:bottom;">'+fdsarc_infoText+' <span onclick="fdsarc_setOptionOn();" title="'+fdsarc_onOptionTitle+'" style="cursor:pointer;"><input type="radio" name="fdsarc_OnOptionRadio" value="on" onclick="fdsarc_setOptionOn()" onFocus="this.blur();" style="vertical-align:bottom; cursor:pointer; margin:0 1px 0 3px;" />'+fdsarc_onOptionText+'</span>&nbsp;<span onclick="fdsarc_setOptionOff();" title="'+fdsarc_offOptionTitle+'" style="cursor:pointer;"><input type="radio" name="fdsarc_OffOptionRadio" value="off" onclick="fdsarc_setOptionOff();" onFocus="this.blur();" style="vertical-align:bottom; cursor:pointer; margin:0 1px 0 3px;" />'+fdsarc_offOptionText+'</span></div></td>');
 document.write('</tr></tbody></table>');
-document.write('<table width="100%" ><tr>');
-document.write('<th scope="col" width="50%"><div align="left" style="font-size:8px"><a href="http://blogandtipsblog.blogspot.com/2011/09/how-to-install-auto-reloadrefresh-on.html" align="left" title="Get Script to Change Your Free Cbox Being Cbox Auto Reload" target="_blank">Get This</a></div></th>');
 document.write('<th scope="col" width="50%"><div align="right" style="font-size:8px"><a href="'+fdsarc_creditURL+'" align="right" title="'+fdsarc_creditTitle+'" target="_blank">'+fdsarc_creditText+'</a></div></th>');
 document.write('</tr></table>');
 document.write('</div>');
@@ -48,15 +46,6 @@ document.write('<sty'+'le>'
 +'#fdsarc_footer a,#fdsarc_footer a:visited '+'{  } '
 +'#fdsarc_footer a:hover '+'{  } ');
 document.write('</sty'+'le>');
-document.write('<div id="fdsarc_footer">'
-+'<a href="#">Free Full Fownload</a>'
-+'<br/>'
-+'<a href="#">Blog and Tips Blog</a>'
-+'<br/>'
-+'<a href="#">Free Games Storage</a>'
-+'<br/>'
-+'<a href="#">Android APk Free</a>'
-+'</div>');
 }
 function fdsarc_setOptionOn() {
 clearTimeout(fdsarc_loopRefresh);
